2b53f1cf0cba328b0559893084576acc5abe132b
Bhargav Radadiya <br98982931@gmail.com>
2026-02-10 23:05:50 +0530

    bhadvayu bandh ho

--- bhargav.py
+++ bhargav.py
@@ -3,6 +3,10 @@ print("<-- this green line means my code is tracked by GitHub -->")
 print("I am learning GitHub and Python together!")
 def greet(name):
     return f"Hello, {name}! Welcome to GitHub and Python learning journey."
+<<<<<<< HEAD
 print(greet("Bhargav"))
 print("Bhadvayu")
 print("no bhadvayu ho")
+=======
+print(greet("Bhargav"))
+>>>>>>> parent of 3d6d1f6 (bhadvayu ho)
