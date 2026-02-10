d7cb6cbbf2eb437109b1d02d414feb8ccda2ec5f
Bhargav Radadiya <br98982931@gmail.com>
2026-02-10 21:37:57 +0530

    this is changes above commit

--- bhargav.py
+++ bhargav.py
@@ -1,3 +1,6 @@
 
 print("<-- this green line means my code is tracked by GitHub -->")
 print("I am learning GitHub and Python together!")
+def greet(name):
+    return f"Hello, {name}! Welcome to GitHub and Python learning journey."
+print(greet("Bhargav"))
